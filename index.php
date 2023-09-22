dae11913d5e1cc368182f03d72e1bf5b2e9cdd11
Marco Antonio <201914331j@utea.edu.pe>
2023-09-22 15:59:31 -0500

    final

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 $actualsesion = $_SESSION['correo'];
 
 if($actualsesion == null || $actualsesion == ''){
-header('location: includes/_sesion/login.php');
+header('location: includes/sesion/login.php');
 }
 else{
    header('location: views/usuarios/index.php');
